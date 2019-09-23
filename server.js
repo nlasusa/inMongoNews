0fd84a8f65fb8c82853dd20a5aac919dbbc49842
Nicole LaSusa <nicolelasusa@gmail.com>
2019-09-22 22:24:50 -0400

    server started and initial html

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// scraping tools 
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
