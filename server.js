c303ece35afffd60e226e9471fd3e192549e8ea5
Nicole LaSusa <nicolelasusa@gmail.com>
2019-09-26 20:58:24 -0400

    tryingtofixerror

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("app/models");
+var db = require("./models");
 
 var PORT = process.env.PORT ||3000;
 
